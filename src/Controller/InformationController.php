3441d86b5ee9d629272ee8694d406b163361098f
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-05-07 20:38:42 +0000

    Backend

--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/information')]
+#[Route('/backend')]
 class InformationController extends AbstractController
 {
     #[Route('/', name: 'app_information_index', methods: ['GET'])]
