5a0bdce0b21fed66e4cb7fc0307065c06256cfa2
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-06-18 10:28:45 +0000

    Backend

--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Information;
 use App\Form\InformationType;
 use App\Repository\InformationRepository;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,65 +17,113 @@ class InformationController extends AbstractController
     #[Route('/', name: 'app_information_index', methods: ['GET'])]
     public function index(InformationRepository $informationRepository): Response
     {
-        dd($informationRepository->findAll());
+//        dd($informationRepository->findAll());
         return $this->render('information/index.html.twig', [
             'information' => $informationRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_information_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, InformationRepository $informationRepository): Response
+    #[Route('/new', name: 'app_information_statistique', methods: ['GET'])]
+    public function new(InformationRepository $informationRepository): Response
     {
-        $information = new Information();
-        $form = $this->createForm(InformationType::class, $information);
-        $form->handleRequest($request);
+        $datas = $informationRepository->findAll();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $information->setEtape(1);
-            $informationRepository->save($information, true);
-
-            return $this->redirectToRoute('app_information_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('information/new.html.twig', [
-            'information' => $information,
-            'form' => $form,
+        return $this->render('information/new.html.twig',[
+            'developpements' => $this->developpement($datas),
+            'connaissances' => $this->connaissance($datas),
+            'rotary' => $this->rotary($datas)
         ]);
     }
 
-    #[Route('/{id}', name: 'app_information_show', methods: ['GET'])]
-    public function show(Information $information): Response
+    private function developpement($datas): array
     {
-        return $this->render('information/show.html.twig', [
-            'information' => $information,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_information_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Information $information, InformationRepository $informationRepository): Response
-    {
-        $form = $this->createForm(InformationType::class, $information);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $informationRepository->save($information, true);
-
-            return $this->redirectToRoute('app_information_index', [], Response::HTTP_SEE_OTHER);
+        $developpements=[];
+        foreach ($datas as $data){
+            $developpements[] = $data->getBesoinFormationDevPro();
         }
 
-        return $this->renderForm('information/edit.html.twig', [
-            'information' => $information,
-            'form' => $form,
-        ]);
-    }
+        $occurences = []; $nb=0;
+        foreach ($developpements as $developpement){
+            foreach ($developpement as $item){
+                if (isset($occurences[$item])) $occurences[$item]++;
+                else $occurences[$item] = 1;
 
-    #[Route('/{id}', name: 'app_information_delete', methods: ['POST'])]
-    public function delete(Request $request, Information $information, InformationRepository $informationRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $information->getId(), $request->request->get('_token'))) {
-            $informationRepository->remove($information, true);
+                $nb++;
+            }
         }
 
-        return $this->redirectToRoute('app_information_index', [], Response::HTTP_SEE_OTHER);
+        $FormDev=[]; if (!$nb) $nb=1;
+        foreach ($occurences as $element => $nombre){
+            $pourcentage = round($nombre * 100 / $nb, 1);
+            $FormDev[] = [
+                'element' => $element,
+                'nombre' => "$nombre",
+                'pourcentage' => $pourcentage
+            ];
+        }
+
+        return $FormDev;
+    }
+
+    private function connaissance($datas)
+    {
+        $connaissances = [];
+        foreach ($datas as $data){
+            $connaissances[] = $data->getBesoinConnaissanceActivitePro();
+        }
+
+        $occurences=[]; $nb=0;
+        foreach ($connaissances as $connaissance){
+            foreach ($connaissance as $item){
+                if (isset($occurences[$item])) $occurences[$item]++;
+                else $occurences[$item] = 1;
+
+                $nb++;
+            }
+        }
+
+        $formConnaissance=[]; if (!$nb) $nb=1;
+        foreach ($occurences as $element => $nombre){
+            $pourcentage = round($nombre * 100 / $nb, 1);
+            $formConnaissance[] = [
+                'element' => $element,
+                'nombre' => "$nombre",
+                'pourcentage' => $pourcentage
+            ];
+        }
+
+//        dd($formConnaissance);
+
+        return $formConnaissance;
+    }
+
+    private function rotary($datas)
+    {
+        $connaissances = [];
+        foreach ($datas as $data){
+            $connaissances[] = $data->getBesoinFormationRotary();
+        }
+
+        $occurences=[]; $nb=0;
+        foreach ($connaissances as $connaissance){
+            foreach ($connaissance as $item){
+                if (isset($occurences[$item])) $occurences[$item]++;
+                else $occurences[$item] = 1;
+
+                $nb++;
+            }
+        }
+
+        $formConnaissance=[]; if (!$nb) $nb=1;
+        foreach ($occurences as $element => $nombre){
+            $pourcentage = round($nombre * 100 / $nb, 1);
+            $formConnaissance[] = [
+                'element' => $element,
+                'nombre' => "$nombre",
+                'pourcentage' => $pourcentage
+            ];
+        }
+
+        return $formConnaissance;
     }
 }
